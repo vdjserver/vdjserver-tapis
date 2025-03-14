5f5f011a757637def571ffded05c147bed787576
bcorrie <bdcorrie@gmail.com>
2025-03-14 14:11:22 +0000

    Remove code from statistics, add some comments

--- apps/compairr/ls6/compairr_common.sh
+++ apps/compairr/ls6/compairr_common.sh
@@ -52,39 +52,10 @@ function run_compairr_workflow() {
     fileBasename="${file%.*}" # file.airr.tsv -> file.airr
     fileBasename="${fileBasename%.*}" # file.airr -> file
 
-    # Rearrangement counts
+    # Run compairr in matrix mode, using the "analysis_type" to determine
+    # which method to use.
     if [[ "$file_type" == "rearrangement" ]] ; then
 	apptainer exec -e ${compairr_image} compairr -f -e -u -s $analysis_type --matrix ${file} --out $fileBasename.matrix.tsv
-        #apptainer exec -e ${compairr_image} python3 count_statistics.py $file
     fi
 
-    return
-    # Clonal abundance
-    if [[ "$file_type" == "clone" ]] ; then
-        apptainer exec -e -B $PWD:/data ${compairr_image} /data/clonal_abundance.R -d $file -o $fileBasename
-    fi
-
-    # Gene Usage
-    if [[ "$file_type" == "clone" ]] ; then
-        apptainer exec -e -B $PWD:/data ${compairr_image} /data/gene_usage.R -d $file -o $fileBasename
-    fi
-    if [[ "$file_type" == "rearrangement" ]] ; then
-        $PYTHON repcalc_create_config.py --init gene_usage_template.json ${metadata_file} --rearrangementFile $file --repertoireID ${repertoire_id} --germline ${germline_db} gene_usage_config.json
-        apptainer exec -e ${compairr_image} repcalc gene_usage_config.json
-    fi
-
-    # Amino Acid properties
-    #singularity exec -e -B $PWD:/data ${compairr_image} /data/aa_properties.R -d $file
-
-    # Junction length distribution
-    $PYTHON repcalc_create_config.py --init junction_length_template.json ${metadata_file} --rearrangementFile $file --repertoireID ${repertoire_id} --germline ${germline_db} junction_length_config.json
-    apptainer exec -e ${compairr_image} repcalc junction_length_config.json
-
-    # Diversity curve
-    if [[ "$file_type" == "clone" ]] ; then
-        apptainer exec -e -B $PWD:/data ${compairr_image} /data/diversity_curve.R -d $file -o $fileBasename
-    fi
-    
-    # final report
-    $PYTHON rearrangement_report.py ${repertoire_id} null null $file
 }
