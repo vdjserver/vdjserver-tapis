71946d08823e23eb607ad5ba7b83dc13a53d3431
Tanzira Najnin <najnintogo@gmail.com>
2025-05-01 18:40:33 -0500

    added functionality for downloading both folder and file

--- python/vdjserver/files.py
+++ python/vdjserver/files.py
@@ -196,26 +196,37 @@ def tapis_files_revoke_permission(path, username, system_id=None, token=None):
     except Exception as e:
         print(f"Error: {str(e)}")
         
-def tapis_files_download(path, system_id = None, token = None, output_filename=None, chunk_size=128*1024):
+def tapis_files_download(path, system_id=None, token=None, output_filename=None, chunk_size=128 * 1024):
     if system_id is None:
         system_id = vdjserver.defaults.storage_system_id
-    if token == None:
+    if token is None:
         token = os.environ['JWT']
+    
     # Construct the Tapis file download URL (for Agave-style APIs)
     url = f"https://vdjserver.tapis.io/v3/files/content/{system_id}/{path}"
     headers = {"X-Tapis-Token": token}
     if output_filename is None:
-        output_filename = path.split('/')[-1]  # Extract filename from the path if not provided
+        output_filename = path.split('/')[-1]
 
     downloaded_size = 0  # Variable to track downloaded size
     start_time = time.time()  # Start time to calculate download speed
+    
     try:
-        print(f"Downloading {output_filename} in chunks...")
-        # Make the GET request with stream=True to download in chunks # current chunksize 128KB
-        response = requests.get(url, headers=headers, stream=True)
+        tapis_obj = vdjserver.defaults.init_tapis(token)
+        contents = tapis_obj.files.getStatInfo(systemId=system_id, path=path)
+        if contents.get('dir'):
+            response = requests.get(url, headers=headers, stream=True, params={'zip': 'true'})
+            print(f"Downloading {output_filename} as ZIP...")
+            output_filename = f'{output_filename}.zip'
+        else:
+            # Assuming the server supports the zip parameter
+            response = requests.get(url, headers=headers, stream=True)
+        
+        # Check if the response status code is OK
         if response.status_code != 200:
             print(f"Error: {response.text}")
             return
+
         # Open file and write in chunks
         with open(output_filename, 'wb') as f:
             for chunk in response.iter_content(chunk_size=chunk_size):
@@ -231,9 +242,10 @@ def tapis_files_download(path, system_id = None, token = None, output_filename=N
                         print(f"Downloaded: {downloaded_size / 1024 / 1024:.2f} MB at {speed:.2f} MB/min", end='\r')
 
         print(f"\nDownload complete: {output_filename}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading file: {e}")
+    except Exception as e:
+        print("-------------------------------------------------")
+        print(f"Error downloading file: \n{e}")
+        print("-------------------------------------------------")
 
 def postits_list_cmd(uuid, system_id=None, token=None):
     if system_id is None:
