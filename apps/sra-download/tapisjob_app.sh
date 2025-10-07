1b72fa2fb8cb0efca1e84a77fceafb3ff0d37cc2
Tanzira Najnin <najnintogo@gmail.com>
2025-10-07 14:35:08 -0500

    Finished implementing SRA download app for multiple SRA run ids.

--- apps/sra-download/tapisjob_app.sh
+++ apps/sra-download/tapisjob_app.sh
@@ -16,7 +16,7 @@
 printf "START at $(date)\n\n"
 
 echo "Application parameters:"
-echo SRA_run_id="${SRA_run_id}"
+echo SRA_run_ids="${SRA_run_ids}"
 echo project_id="${project_id}"
 echo split_flag=${split_flag}
 
@@ -30,25 +30,46 @@ source ${HOME}/sra-download/bin/activate
 source ${HOME}/.ssh/sra-download.env
 
 dest_folder="/projects/${project_id}/files/"
-docker run -v /vdjZ:/work --user vdj:G-803419 vdjserver/sra-tools:3.2.1 fasterq-dump "${SRA_run_id}" $split_option -t /work${dest_folder} --outdir /work${dest_folder}
+for SRA_run_id in $SRA_run_ids; do
+    echo "Processing ${SRA_run_id}..."
 
-# Compress and remove .fastq files
-if [ "${split_flag}" -eq 1 ]; then
-    echo "Compressing and cleaning up paired-end FASTQ files..."
-    gzip -f "/vdjZ/${dest_folder}${SRA_run_id}_1.fastq" 
-    gzip -f "/vdjZ/${dest_folder}${SRA_run_id}_2.fastq"
-    for i in 1 2; do
-        file="${SRA_run_id}_${i}.fastq.gz"
+    docker run -v /vdjZ:/work --user vdj:G-803419 vdjserver/sra-tools:3.2.1 fasterq-dump "${SRA_run_id}" $split_option -t /work${dest_folder} --outdir /work${dest_folder}
+    # Checking for fasterq-dump error
+    if [ $? -ne 0 ]; then
+        echo "ERROR: fasterq-dump failed for ${SRA_run_id}. Skipping."
+        continue
+    fi
+    # Compress and remove .fastq files
+    if [ "${split_flag}" -eq 1 ]; then
+        f1="/vdjZ/${dest_folder}${SRA_run_id}_1.fastq"
+        f2="/vdjZ/${dest_folder}${SRA_run_id}_2.fastq"
+        if [ ! -f "$f1" ] || [ ! -f "$f2" ]; then
+            echo "ERROR: Expected files not found for paired-end ${SRA_run_id}. Skipping"
+            continue
+        fi
+        echo "Compressing and cleaning up paired-end FASTQ files..."
+        gzip -f "$f1"
+        gzip -f "$f2"
+        for i in 1 2; do
+            file="${SRA_run_id}_${i}.fastq.gz"
+            vdjserver-tools project file-attach ${project_id} ${file} --file-type 2
+        done
+    else
+        echo "Compressing and cleaning up single-end FASTQ file..."
+        f="/vdjZ/${dest_folder}${SRA_run_id}.fastq"
+        if [ ! -f "$f" ]; then
+            echo "ERROR: Expected single end file not found for ${SRA_run_id}. Skipping."
+            continue
+        fi
+        echo "Compressing and cleaning up single-end FASTQ file..."
+        gzip -f "$f"
+        file="${SRA_run_id}.fastq.gz"
         vdjserver-tools project file-attach ${project_id} ${file} --file-type 2
-    done
-else
-    echo "Compressing and cleaning up single-end FASTQ file..."
-    gzip -f "/vdjZ/${dest_folder}${SRA_run_id}.fastq" 
-    file="/vdjZ/${dest_folder}${SRA_run_id}.fastq.gz"
-    vdjserver-tools project file-attach ${project_id} ${file} --file-type 2
-fi
+    fi
 
-printf "DONE at $(date)\n\n"
+    echo "Done for ${SRA_run_id}"
+done
+printf "All DONE at $(date)\n\n"
 
 # fileTypeCodes: {
 #         FILE_TYPE_UNSPECIFIED: 0,
@@ -62,4 +83,4 @@ printf "DONE at $(date)\n\n"
 #         FILE_TYPE_VDJML: 8,
 #         FILE_TYPE_AIRR_TSV: 9,
 #         FILE_TYPE_AIRR_JSON: 10,
-#     },
+#     }
